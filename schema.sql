b87ddf33908dec027594100fe2b5821ae9edddda
Yiwen Zhu <yiwzhu@seas.upenn.edu>
2020-12-16 23:16:00 +0000

    Add two more columns to schema

--- schema.sql
+++ schema.sql
@@ -2,51 +2,11 @@ DROP DATABASE IF EXISTS join_us;
 CREATE DATABASE join_us;
 USE join_us;
 
-CREATE TABLE users (
+CREATE TABLE members (
 	email VARCHAR(255) PRIMARY KEY,
-	country VARCHAR(255) NOT NULL, 
+	first_name VARCHAR(255), 
+	gender VARCHAR(255) NOT NULL,
+	age INT NOT NULL,
 	created_at TIMESTAMP DEFAULT NOW()
 );
-
--- Some 
-/*
-SELECT
-	DATE_FORMAT(created_at, '%M %D %Y') AS earliest_date 
-FROM users 
-ORDER BY created_at
-LIMIT 1;
-
-SELECT
-	email, 
-	created_at
-FROM users
-ORDER BY created_at
-LIMIT 1;
-
-SELECT 
-	MONTHNAME(created_at) AS month, # DATE_FORMAT(created_at, '%M')
-	COUNT(*) AS count
-FROM users
-GROUP BY month
-ORDER BY count DESC;
-
-SELECT
-	COUNT(*) AS yahoo_users
-FROM users
-WHERE email LIKE '%@yahoo.com';
-
-SELECT 
-	CASE
-		WHEN email LIKE '%@gmail.com' THEN 'gmail'
-		WHEN email LIKE '%@yahoo.com' THEN 'yahoo'
-		WHEN email LIKE '%@hotmail.com' THEN 'hotmail'
-		ELSE 'other'
-	END AS provider, 
-	COUNT(*) AS total_users
-FROM users
-GROUP BY provider
-ORDER BY total_users DESC; 
-*/
-
-
 
