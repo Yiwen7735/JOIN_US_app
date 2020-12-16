06b418b3c91ccf32615a7796a47965420126fa24
Yiwen Zhu <yiwzhu@seas.upenn.edu>
2020-12-16 21:35:05 +0000

    Add part 2: take user input (signing up)

---
+++ app2.js
@@ -0,0 +1,55 @@
+/*
+This is the 'main' file that
+displays homepage, takes user input & updates sql database
+*/
+//packages required
+var mysql = require('mysql'); 
+var express = require('express'); 
+var http = require('http');      
+var parser = require('body-parser');
+
+var app = express();
+app.set("view engine", "ejs"); //include html file
+app.use(parser.urlencoded({extended: true}));   //parse data from html body 
+app.use(express.static(__dirname + "/public")); //include css file
+
+//build connection with mysql
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root', 
+	password: process.env.mysql_pw, 
+	database: 'join_us'
+});
+
+//homepage
+app.get("/", function(req, res){
+	var q = "SELECT COUNT(*) AS count FROM users";
+	connection.query(q, function(error, results){
+		if (error) throw error;
+		res.render("home", {count: results[0].count}); //call the home.ejs file
+	});
+});
+
+//post register
+app.post("/register", function(req, res){
+	var user = {
+		email: req.body.email, 
+		country: req.body.country
+	};
+	connection.query("INSERT INTO users SET ?", user, function(error, results){
+		if (error) throw error;
+		res.redirect("/"); //redirect to homepage after signing up
+	});
+});
+
+/*
+//testing
+app.get("/lucky_num", function(req, res){
+	var rnum = Math.floor(Math.random() * 10) + 1;
+	res.send("Your lucky number is " + rnum);
+})
+*/
+http.createServer(app).listen(80);
+
+
+
